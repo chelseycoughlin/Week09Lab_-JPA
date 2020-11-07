2911e8199de6a7d45448777ea9f38af6c164fd34
chelseycoughlin <chelsey.coughlin@edu.sait.ca>
2020-11-07 15:14:31 -0700

    Lab 09 Complete

---
+++ src/java/database/DBUtil.java
@@ -0,0 +1,13 @@
+package database;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+    private static final EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("Week09LabPU");
+
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
+}
---
+++ src/java/services/UserService.java
@@ -0,0 +1,50 @@
+package services;
+
+import database.UserDB;
+import models.Users;
+import java.util.List;
+
+public class UserService {
+
+    private UserDB userDB;
+
+    public UserService() {
+        userDB = new UserDB();
+    }
+
+    public Users get(String username) throws Exception {
+        return userDB.getUser(username);
+    }
+
+    public List<Users> getAll() throws Exception {
+        return userDB.getAll();
+    }
+
+    public int update(String username, String password, String firstname, String lastname, String email) throws Exception {
+        Users user = get(username);
+        user.setPassword(password);
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        return userDB.update(user);
+    }
+
+    public int delete(String username) throws Exception {
+        Users deletedUser = userDB.getUser(username);
+        // do not allow the admin to be deleted
+        if (deletedUser.getUsername().equals("admin")) {
+            return 0;
+        }
+        return userDB.delete(deletedUser);
+    }
+
+    public int insert(String username, String password, String firstname, String lastname, String email) throws Exception {
+        Users user = new Users();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        return userDB.insert(user);
+    }
+}
